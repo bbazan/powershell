1b4e52b075d4f07e50597dd16e15a708f5d25256
bbazan <35705603+bbazan@users.noreply.github.com>
2020-03-16 15:00:56 -0400

    Adding in alarm suppression script ESXi/vCenter
    Suppresses alarms on hosts for: 
    This host is potentially vulnerable to issues described in CVE-2018-3646, please refer to https://kb.vmware.com/s/article/55636 for details and VMware recommendations. KB 55636

---
+++ vcenter-esxi-suppress-alarm.ps1
@@ -0,0 +1,18 @@
+﻿#Author Brandon Bazan http://ifitisnotbroken.wordpress.com/ 2020
+
+Write-Host -ForegroundColor Yellow "Assembling a list of hosts to suppress warnings against"
+$hosts = Get-VMHost
+#ForEach loop that gets the current state of the alarm and updates to suppress the alarm
+#Future feature will be to add in a menu allowing you to state the alarm "UserVars" and value
+ForEach($alarmhost in $hosts){
+    Write-host -ForegroundColor Green "UserVars.SuppressHyperthreadWarning is being supressed on $host"
+    #Changing the value of this UserVars to 1 which suppresses the alarm, adding "Confirm:$false" which does not prompt the user to confirm the alarm suppression
+    Get-AdvancedSetting -Entity $alarmhost -Name UserVars.SuppressHyperthreadWarning | Set-AdvancedSetting -Value 1 -Confirm:$false -Verbose
+
+    #Writing the final status of changed alarms
+    $UpdatedValue = Get-AdvancedSetting -Entity $alarmhost -Name UserVars.SuppressHyperthreadWarning
+    Write-Host -ForegroundColor Yellow " `n ************************************************************* `n"
+    Write-Host -ForegroundColor Green "Updated Values 1 = Suppressed, 0 = Active Alarm `n"
+    Write-Host -ForegroundColor Green " ---- $updatedvalue ---- `n"
+    Write-Host -ForegroundColor Yellow " `n ************************************************************* `n"
+}
